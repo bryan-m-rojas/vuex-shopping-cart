0a8738c8ed278b05ac87922c685691c895f30d04
bryan-m-rojas <bmr333@nyu.edu>
2020-08-18 17:06:42 -0400

    vuex getters

--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,8 @@ export default new Vuex.Store({
   },
 
   getters: { // equivalent to computed properties
-    productsCount () {
-      // ...
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0)
     }
   },
 
