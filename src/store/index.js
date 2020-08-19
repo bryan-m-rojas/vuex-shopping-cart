6a414bc6b6244cc754e8193dedfb1038f51d1335
bryan-m-rojas <bmr333@nyu.edu>
2020-08-18 21:39:31 -0400

    Vuex Modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,78 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import actions from '@/store/action'
+import cart from '@/store/modules/cart'
+import products from '@/store/modules/products'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    cart,
+    products
+  },
+
   state: { // equivalent to data
-    products: [],
-    // {id, qunatity}
-    cart: [],
-    checkoutStatus: null
   },
 
   getters: { // equivalent to computed properties
-    availableProducts (state, getters) {
-      return state.products.filter(product => product.inventory > 0)
-    },
-
-    cartProducts (state) {
-      return state.cart.map(cartItem => {
-        const product = state.products.find(product => product.id === cartItem.id)
-        return {
-          title: product.title,
-          price: product.price,
-          quantity: cartItem.quantity
-        }
-      })
-    },
-
-    cartTotal (state, getters) {
-      // let total = 0
-      // getters.cartProducts.forEach(product => {
-      //   total += product.price * product.quantity
-      // })
-      // return total
-      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
-    },
-
-    productIsInStock () {
-      return (product) => {
-        return product.inventory > 0
-      }
-    }
   },
 
   actions,
 
   mutations: { // new - no equivalent
-    setProducts (state, products) {
-      // update products
-      state.products = products
-    },
-
-    pushProductToCart (state, productId) {
-      state.cart.push({
-        id: productId,
-        quantity: 1
-      })
-    },
-
-    incrementItemQuantity (state, cartItem) {
-      cartItem.quantity++
-    },
-
-    decrementProductInventory (state, product) {
-      product.inventory--
-    },
-
-    setCheckoutStatus (state, status) {
-      state.checkoutStatus = status
-    },
-
-    emptyCart (state) {
-      state.cart = []
-    }
   }
 })
